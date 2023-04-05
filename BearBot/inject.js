fa06fdf93f5a1c27d87f7a8cb72625b048cd63a1
Youssef Ismail <yismail@ualberta.ca>
2023-04-04 20:21:35 -0600

    The revolution!

--- BearBot/inject.js
+++ BearBot/inject.js
@@ -1,10 +1,103 @@
-/**
- * injectScript - Inject internal script to available access to the `window`
- *
- * @param  {type} file_path Local path of the internal script.
- * @param  {type} tag The tag as string, where the script will be append (default: 'body').
- * @see    {@link http://stackoverflow.com/questions/20499994/access-window-variable-from-content-script}
- */
+const TIMEOUT_IN_MS = 5 * 1000; // 5 seconds
+
+let interval;
+let localRunning = false;
+
+chrome.storage.local.onChanged.addListener(async (changes) => {
+  clearInterval(interval);
+
+  if (changes.started?.newValue) {
+    interval = setInterval(onInterval, TIMEOUT_IN_MS);
+  }
+});
+
+chrome.storage.local.get("submit", (ls) => {
+  if (ls.submit && !localRunning) {
+    console.log("Submit onload.");
+    handleSubmit();
+    localRunning = true;
+  }
+});
+
+chrome.storage.local.get("started", (ls) => {
+  if (ls.started) {
+    console.log("Started onload.");
+    interval = setInterval(onInterval, TIMEOUT_IN_MS);
+  }
+});
+
+async function onInterval() {
+  console.log("Interval.");
+  if (!localRunning) {
+    if (await findOpenSeats()) {
+      await chrome.storage.local.set({ submit: true });
+      clearInterval(interval);
+    }
+
+    location.reload();
+  }
+}
+
+async function handleSubmit() {
+  let iterations = 0;
+  let searchInterval = setInterval(() => {
+    console.log("Searching for submit button...", iterations);
+    let submitButton = document.getElementById("SSR_ENRL_FL_WRK_SUBMIT_PB");
+
+    if (submitButton) {
+      console.log("Found submit button");
+      clearInterval(interval);
+      clearInterval(searchInterval);
+
+      let event = new CustomEvent("submit-bearbot");
+
+      document.getElementsByTagName("body")[0].dispatchEvent(event);
+
+      chrome.storage.local.set({ submit: false });
+    }
+
+    if (iterations > 10) {
+      clearInterval(searchInterval);
+    }
+
+    iterations++;
+  }, 1000);
+}
+
+async function findOpenSeats() {
+  const rows = document.getElementsByTagName("tr");
+  const ls = await chrome.storage.local.get("filter");
+  const sections = ls.filter;
+
+  console.log("Filter:", sections);
+
+  for (const row of rows) {
+    const html = row.innerHTML;
+
+    const canCheck =
+      html.includes("Open Seats") &&
+      (!sections.length || sections.some((section) => html.includes(section)));
+
+    if (canCheck) {
+      const idx = Array.from(row.getElementsByTagName("a"))
+        .find((a) => a.innerText.includes("Enroll"))
+        ?.href.split("$")[1]
+        .split("');")[0];
+
+      if (!idx) return false;
+
+      const enrollEvent = new CustomEvent("enroll-bearbot", { detail: idx });
+
+      document.getElementsByTagName("body")[0].dispatchEvent(enrollEvent);
+
+      return true;
+    }
+  }
+  return false;
+}
+
+// Get the window to the soul
+// http://stackoverflow.com/questions/20499994/access-window-variable-from-content-script}
 function injectScript(file_path, tag) {
   var node = document.getElementsByTagName(tag)[0];
   var script = document.createElement("script");
@@ -12,27 +105,5 @@ function injectScript(file_path, tag) {
   script.setAttribute("src", file_path);
   node.appendChild(script);
 }
+
 injectScript(chrome.runtime.getURL("event.js"), "body");
-
-chrome.runtime.sendMessage({ type: "popupStatus" }, (isPopupOpen) => {
-  if (isPopupOpen && chrome.runtime.lastError === undefined) {
-    let iterations = 0;
-    let interval = setInterval(() => {
-      let submitButton = document.getElementById("SSR_ENRL_FL_WRK_SUBMIT_PB");
-
-      if (submitButton) {
-        clearInterval(interval);
-
-        let event = new CustomEvent("submit-bearbot");
-
-        document.getElementsByTagName("body")[0].dispatchEvent(event);
-      }
-
-      if (iterations > 10) {
-        clearInterval(interval);
-      }
-
-      iterations++;
-    }, 1000);
-  }
-});
--- BearBot/popup.js
+++ BearBot/popup.js
@@ -1,113 +1,38 @@
 const startButton = document.getElementById("startButton");
 const stopButton = document.getElementById("stopButton");
-const successMsg = document.getElementById("successMsg");
 const sectionInput = document.getElementById("sectionInput");
 
-const INTERVAL_TIME = 5 * 1000; // 5 seconds
-
-const beep = new Audio(chrome.runtime.getURL("beep.mp3"));
-beep.loop = true;
-
-let isPopupOpen = false;
-
-chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
-  if (message.type === "popupStatus") {
-    sendResponse(isPopupOpen);
+chrome.storage.local.get(["started"], (result) => {
+  if (result.started) {
+    toggleVisibility();
   }
-});
 
-window.addEventListener("load", () => {
-  isPopupOpen = true;
-});
-
-window.addEventListener("unload", () => {
-  isPopupOpen = false;
-});
-
-let interval;
-startButton.onclick = () => {
-  toggleVisibility();
-
-  interval = setInterval(() => {
-    chrome.tabs.reload();
-    checkOpen();
-  }, INTERVAL_TIME);
-};
-
-stopButton.onclick = () => {
-  toggleVisibility();
-
-  beep.pause();
-
-  clearInterval(interval);
-};
-
-async function checkOpen() {
-  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  chrome.scripting.executeScript(
-    {
-      target: { tabId: tab.id },
-      func: findOpenSeats,
-      args: [sectionInput.value],
-    },
-    (injectionResults) => {
-      if (injectionResults === undefined) return;
-      for (const frameResult of injectionResults) {
-        if (frameResult.result) {
-          onFound();
-        }
-      }
-    }
-  );
-}
-
-async function findOpenSeats(sectionInputValue) {
-  const rows = document.getElementsByTagName("tr");
-  const sections = sectionInputValue && sectionInputValue.split(" ");
-
-  for (const row of rows) {
-    const html = row.innerHTML;
-
-    const canCheck =
-      !sections || sections.some((section) => html.includes(section));
-
-    if (canCheck && html.includes("Open Seats")) {
-      const idx = Array.from(row.getElementsByTagName("a"))
-        .find((a) => a.innerText.includes("Enroll"))
-        ?.href.split("$")[1]
-        .split("');")[0];
-
-      if (!idx) return false;
-
-      const enrollEvent = new CustomEvent("enroll-bearbot", { detail: idx });
-
-      document.getElementsByTagName("body")[0].dispatchEvent(enrollEvent);
-
-      return true;
-    }
-  }
-  return false;
-}
-
-function onFound() {
-  successMsg.classList.remove("invisible");
-
-  beep.play();
-
-  chrome.notifications.create("CLASS_FOUND", {
-    type: "basic",
-    iconUrl: "bear.png",
-    title: "BearBot: SEAT ALERT",
-    message: "An open seat for your class was found!",
-    priority: 2,
+  chrome.action.setBadgeText({ text: result.started ? "ON" : "OFF" });
+  chrome.action.setBadgeBackgroundColor({
+    color: result.started ? "#cfffd5" : "#faa0a0",
   });
+});
 
-  clearInterval(interval);
-}
+startButton.onclick = async () => {
+  toggleVisibility();
+
+  await chrome.storage.local.set({
+    filter: sectionInput.value ? sectionInput.value.split(" ") : [],
+  });
+  await chrome.storage.local.set({ started: true });
+  await chrome.action.setBadgeText({ text: "ON" });
+  await chrome.action.setBadgeBackgroundColor({ color: "#cfffd5" });
+};
+
+stopButton.onclick = async () => {
+  toggleVisibility();
+
+  await chrome.storage.local.set({ started: false });
+  await chrome.action.setBadgeText({ text: "OFF" });
+  await chrome.action.setBadgeBackgroundColor({ color: "#faa0a0" });
+};
 
 function toggleVisibility() {
   startButton.classList.toggle("invisible");
   stopButton.classList.toggle("invisible");
-
-  successMsg.classList.add("invisible");
 }
